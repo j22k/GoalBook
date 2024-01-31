d2122278c72df8ef47632de619e8e72714d6cbd0
Junaid <junaidkaliyadan@gmail.com>
2024-01-31 15:45:02 +0530

    Login Page

--- client/client/src/App.js
+++ client/client/src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom'; // Make sure to import useNavigate if you are using it with React Router
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [username, setUserID] = useState();
+  const [password, setPassword] = useState();
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleSignin = async (e) => {
+    console.log(username, password);
+    console.log(`Login attempt with username: ${username} and password: ${password}`);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Auth-form-container">
+      <form className="Auth-form" onSubmit={handleSignin}>
+        <div className="Auth-form-content">
+          <h3 className="Auth-form-title">Sign In</h3>
+          <div className="form-group mt-3">
+            <label>User ID</label>
+            <input
+              type="text"
+              className="form-control mt-1"
+              placeholder="Enter User ID"
+              required=""
+              onChange={(e) => setUserID(e.target.value)}
+            />
+          </div>
+          <div className="form-group mt-3">
+            <label>Password</label>
+            <input
+              type="password"
+              className="form-control mt-1"
+              placeholder="Enter password"
+              required=""
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="d-grid gap-2 mt-3">
+            <button type="button" onClick={handleSignin} className="btn btn-success">
+              Submit
+            </button>
+          </div>
+          <p className="forgot-password text-right mt-2">{/* Forgot <a href="#">password?</a> */}</p>
+        </div>
+      </form>
     </div>
   );
 }
