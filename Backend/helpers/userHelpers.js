190735223b37a41cc6cb89fdd65c34ba106fef22
Junaid <junaidkaliyadan@gmail.com>
2024-02-01 12:30:44 +0530

    super admin registration

--- Backend/helpers/userHelpers.js
+++ Backend/helpers/userHelpers.js
@@ -4,26 +4,27 @@ const bcrypt = require('bcrypt');
 
 module.exports = {
   SignIn: async (userData) => {
-      try {
-        const user = await db.getDatabase().collection(collections.USER).findOne({ userid: userData.userID });
+    try {
+      const user = await db.getDatabase().collection(collections.USER).findOne({ userid: userData.userID });
 
-        if (!user) {
-            return { status: false, message: 'Invalid username ' };
-          
-        }
+      if (!user) {
+        return { status: false, message: 'Invalid username ' };
 
-        const matched = await bcrypt.compare(userData.password, user.password);
-
-        if (matched) {
-            delete user.password;
-            console.log("User found");
-            return { status: true, user: user };
-          } else {
-            return { status: false, message: 'Invalid password' };
-          }
-      } catch (error) {
-        console.error(error);
       }
-    
+
+      const matched = await bcrypt.compare(userData.password, user.password);
+
+      if (matched) {
+        delete user.password;
+        console.log("User found");
+        return { status: true, user: user };
+      } else {
+        return { status: false, message: 'Invalid password' };
+      }
+    } catch (error) {
+      console.error(error);
+    }
+
   },
+ 
 };
--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,7 @@ const PORT = process.env.PORT || 5000;
 const db = require('./config/connection');
 const cors = require('cors');
 const userRouter = require('./routes/user');
+const userHelpers = require('./helpers/superAdmin')
 app.use(express.json());
 
 // Use CORS middleware before defining routes
@@ -11,11 +12,32 @@ app.use(cors({
   origin: 'http://localhost:3000',
 }));
 
-// Connect to the database
-db.connectToDatabase();
+
 
 // Use the userRouter for the '/user' route
 app.use('/user', userRouter);
 
 // Start the server
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, async () => {
+  console.log(`Server started on port ${PORT}`);
+
+  // Connect to the database
+  await db.connectToDatabase();
+
+  // Register superadmin upon server start
+  const superadmin = {
+    username: 'super',
+    password: 'super@123',
+  };
+
+  try {
+    result = await userHelpers.CreateSuperAdmin(superadmin);
+    if (result.status) {
+      console.log(result.message);
+    } else {
+      console.log(result.message);
+    }
+  } catch (error) {
+    console.error('Error registering superadmin:', error);
+  }
+});
